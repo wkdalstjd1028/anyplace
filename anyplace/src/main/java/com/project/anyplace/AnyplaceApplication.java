bf39d7dad847b82e716b46feeedfb5db1d2fc4ef
wkdalstjd <wkdalstjd1028@gmail.com>
2025-11-04 18:28:20 +0900

    first commit

---
+++ anyplace/src/main/java/com/project/anyplace/AnyplaceApplication.java
@@ -0,0 +1,13 @@
+package com.project.anyplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyplaceApplication.class, args);
+	}
+
+}
---
+++ anyplace/src/test/java/com/project/anyplace/AnyplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.anyplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
